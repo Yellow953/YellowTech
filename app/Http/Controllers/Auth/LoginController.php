195b40ab38c87d7bf153300497d7c38b052a71b3
Yellow <joemazloum953@gmail.com>
2024-10-04 16:57:33 +0300

    composer update + staff redirect fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,41 +9,19 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
     protected function authenticated(Request $request, $user)
     {
-        if ($user->role == 'admin') {
+        if ($user->role == 'admin' || $user->role == 'staff') {
             return redirect()->route('dashboard');
         } else {
             return redirect()->route('home');
         }
     }
 
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
